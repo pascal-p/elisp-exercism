0e8386adb4897e7c55a891de9ce6cf4d05845df3
Pascal P <lacsap_666@yahoo.fr>
2021-09-17 23:42:42 +1200

    rna-transcription complete

---
+++ rna-transcription/rna-transcription.el
@@ -0,0 +1,35 @@
+;;; rna-transcription.el -- RNA Transcription (exercism)
+
+;;; Commentary:
+
+
+
+;;; Code:
+(setq base
+      '(("G" . "C")
+        ("C" . "G")
+        ("T" . "A")
+        ("A" . "U")))
+
+(defun to-rna (dna-strand)
+  "Given a dna-strand converts it to a RNA ones string
+ref. http://ergoemacs.org/emacs/elisp_string_functions.html"
+  (cond
+   ((<= (length dna-strand) 0) "")
+   ((= (length dna-strand) 1) (single-symb dna-strand))
+   (t (multi-symb strand)))
+  )
+
+(defun single-symb (dna-symb)
+  (cdr (assoc dna-symb base))
+  )
+
+(defun multi-symb (dna-strand)
+  ;; (mapconcat 'identity '("a" "b" "c") "") => "abc"
+  (mapconcat 'identity
+             (mapcar 'single-symb
+                     (mapcar 'char-to-string dna-strand)) "")
+  )
+
+(provide 'rna-transcription)
+;;; rna-transcription.el ends here
