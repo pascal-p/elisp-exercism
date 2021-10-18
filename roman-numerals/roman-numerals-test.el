69840e090ef2f222376b7b269ebd9632db2d1320
Pascal P <lacsap_666@yahoo.fr>
2021-10-18 22:34:53 +1300

    roman numerals complete

---
+++ roman-numerals/roman-numerals-test.el
@@ -0,0 +1,85 @@
+;;; roman-numerals-test.el --- Tests for roman-numerals (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(load-file "roman-numerals.el")
+
+(ert-deftest to-roman-1 ()
+  (should (equal (to-roman 1) "I")))
+
+(ert-deftest to-roman-2 ()
+  (should (equal (to-roman 2) "II")))
+
+(ert-deftest to-roman-3 ()
+  (should (equal (to-roman 3) "III")))
+
+(ert-deftest to-roman-4 ()
+  (should (equal (to-roman 4) "IV")))
+
+(ert-deftest to-roman-5 ()
+  (should (equal (to-roman 5) "V")))
+
+(ert-deftest to-roman-6 ()
+  (should (equal (to-roman 6) "VI")))
+
+(ert-deftest to-roman-9 ()
+  (should (equal (to-roman 9) "IX")))
+
+(ert-deftest to-roman-10 ()
+  (should (equal (to-roman 10) "X")))
+
+(ert-deftest to-roman-27 ()
+  (should (equal (to-roman 27) "XXVII")))
+
+(ert-deftest to-roman-48 ()
+  (should (equal (to-roman 48) "XLVIII")))
+
+(ert-deftest to-roman-59 ()
+  (should (equal (to-roman 59) "LIX")))
+
+(ert-deftest to-roman-93 ()
+  (should (equal (to-roman 93) "XCIII")))
+
+(ert-deftest to-roman-141 ()
+  (should (equal (to-roman 141) "CXLI")))
+
+(ert-deftest to-roman-163 ()
+  (should (equal (to-roman 163) "CLXIII")))
+
+(ert-deftest to-roman-402 ()
+  (should (equal (to-roman 402) "CDII")))
+
+(ert-deftest to-roman-575 ()
+  (should (equal (to-roman 575) "DLXXV")))
+
+(ert-deftest to-roman-911 ()
+  (should (equal (to-roman 911) "CMXI")))
+
+(ert-deftest to-roman-1024 ()
+  (should (equal (to-roman 1024) "MXXIV")))
+
+(ert-deftest to-roman-1972 ()
+  (should (equal (to-roman 1972) "MCMLXXII")))
+
+(ert-deftest to-roman-3000 ()
+  (should (equal (to-roman 3000) "MMM")))
+
+(ert-deftest to-roman-2000 ()
+  (should (equal (to-roman 2000) "MM")))
+
+(ert-deftest to-roman-2001 ()
+  (should (equal (to-roman 2001) "MMI")))
+
+(ert-deftest to-roman-2010 ()
+  (should (equal (to-roman 2010) "MMX")))
+
+(ert-deftest to-roman-2100 ()
+  (should (equal (to-roman 2100) "MMC")))
+
+(ert-deftest to-roman-2110 ()
+  (should (equal (to-roman 2110) "MMCX")))
+
+(provide 'roman-numerals)
+;;; roman-numerals-test.el ends here
---
+++ roman-numerals/roman-numerals.el
@@ -0,0 +1,70 @@
+;;; roman-numerals.el --- roman-numerals Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+(setq ya-roman-map
+      '(
+        (1 . "I") (2 . "II") (3 . "III") (4 . "IV") (5 . "V")
+        (6 . "VI") (7 . "VII") (8 . "VIII") (9 . "IX") (10 . "X")
+        (50 . "L") (100 . "C") (500 . "D") (1000 . "M")))
+
+;; limit 3000
+
+(defun to-roman (num)
+  (let* ((p 1000)
+         (pair (divrem num p))
+         (num (car pair))
+         (rem (cdr pair))
+         (roman ""))
+
+    (if (= 0 rem)
+        (update-roman-value roman p num)
+      ;; else
+      (while (> rem 0)
+        (if (> num 0)
+            (setq roman (update-roman-value roman p num))
+          ;; no else
+          )
+        (setq p (/ p 10)
+              num rem
+              pair (divrem num p)
+              num (car pair)
+              rem (cdr pair)
+              ))
+      (concat roman (roman-fn num p)))
+    ))
+
+(defun update-roman-value (roman p num)
+  (cond ((= p 1000) (concat roman (repeat-string (cdr-assoc p) num)))
+        ((or (= p 100) (= p 10)) (concat roman (roman-fn num p)))
+        ((= p 1) (concat roman (cdr-assoc p)))
+        (t roman)))
+
+(defun roman-fn (num p)
+  (cond
+   ((<= num 3) (repeat-string (cdr-assoc p) num))                                   ;; 30 => XXX
+   ((= num 4) (concat (cdr-assoc p) (cdr-assoc (* 5 p))))                           ;; 40 => XL
+   ((< num 9) (concat (cdr-assoc (* 5 p)) (repeat-string (cdr-assoc p) (- num 5)))) ;; 70 => LXX
+   (t (concat (cdr-assoc p) (cdr-assoc (* 10 p))))                                  ;; 90 => XC
+   ))
+
+(defun cdr-assoc (key)
+  (cdr (assoc key ya-roman-map)))
+
+(defun divrem (n d)
+  (if (= 0 d) (throw 'Error "Division by 0")
+    (let ((q (/ n d))
+          (r (% n d)))
+      (cons q r))
+    ))
+
+(defun repeat-string (str num)
+  "input string str must be mono character
+(repeat-string \"X\" 3) => \"XXX\"
+"
+  (make-string num (string-to-char str)))
+
+
+(provide 'roman-numerals)
+;;; roman-numerals.el ends here
