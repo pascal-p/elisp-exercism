1ae8409fcb387ac0b91acb8be5a5d3d4385c8672
Pascal P <lacsap_666@yahoo.fr>
2021-10-24 16:12:25 +1300

    rail-cipher decode complete

--- rail-cipher/rail-cipher-test.el
+++ rail-cipher/rail-cipher-test.el
@@ -6,6 +6,8 @@
 
 (load-file "rail-cipher.el")
 
+
+;; encode
 (ert-deftest test-encode-1-rail ()
   (should (equal "One rail, only one rail" (encode 1 "One rail, only one rail"))))
 
@@ -24,8 +26,64 @@
 ;; exception encode
 (ert-deftest test-encode-wirh-more-rails-than-letter-in-src ()
   (should-error (encode 24 "More rails than letters")))
-;;
 
+
+;; TODO: decode
+(ert-deftest test-decode-empty-msg ()
+  (should (equal "" (decode 5 ""))))
+
+(ert-deftest test-decode-using-1-rail ()
+  (should (equal "ONE RAIL, ONLY ONE RAIL" (decode 1 "ONE RAIL, ONLY ONE RAIL"))))
+
+(ert-deftest test-decode-using-2-rails ()
+  (should (equal "XOXOXOXOXOXOXOXOXO" (decode 2 "XXXXXXXXXOOOOOOOOO"))))
+
+(ert-deftest test-decode-using-3-rails ()
+  (should (equal "THEDEVILISINTHEDETAILS" (decode 3 "TEITELHDVLSNHDTISEIIEA"))))
+
+(ert-deftest test-decode-using-6-rails ()
+  (should (equal "112358132134558914423337761098715972584418167651094617711286"
+                 (decode 6 "133714114238148966225439541018335470986172518171757571896261"))))
+
+;; identity
+(defun all? (pred-fn lst)
+  "return true iff all element of lst verified the predicate function pred-fn"
+  (eval (cons 'and (mapcar pred-fn lst)))
+  )
+
+(ert-deftest test-decode-encode-identity ()
+  (should
+   (all? (lambda (pair) (equal (car pair)
+                               (encode (cdr pair) (decode (cdr pair) (car pair)))))
+         '(("XOXOXOXOXOXOXOXOXO" . 2)
+           ("WEAREDISCOVEREDFLEEATONCE" . 3)
+           ("THEDEVILISINTHEDETAILS" . 3)
+           ("THEDEVILISINTHEDETAILS". 5)
+           ("133714114238148966225439541018335470986172518171757571896261" . 6)))
+   )
+  )
+
+(ert-deftest test-replace-mark ()
+  (should (equal '(("F" "." "O" "." "A" ".")
+                   ("." "O" "." "B" "." "R"))
+                 (replace-mark '(("?" "." "?" "." "?" ".")
+                                 ("." "?" "." "?" "." "?")) '("F" "O" "A" "O" "B" "R")))))
+
+(ert-deftest test-transpose ()
+  (should (equal '(("F" ".") ("." "O") ("O" ".") ("." "B") ("A" ".") ("." "R"))
+                 (transpose '(("F" "." "O" "." "A" ".") ("." "O" "." "B" "." "R"))))))
+
+(ert-deftest test-squeeze ()
+  (should (equal '(("F") ("O") ("O") ("B") ("A") ("R"))
+                 (mapcar 'squeeze-rail '(("F" ".") ("." "O") ("O" ".") ("." "B") ("A" ".") ("." "R")))))
+  )
+
+(ert-deftest test-word-to-list ()
+  (should (equal '("X" "X" "X" "X" "X" "X" "X" "X" "X" "O" "O" "O" "O" "O" "O" "O" "O" "O")
+                 (word-to-list "XXXXXXXXXOOOOOOOOO")))
+  )
+
+;; other test helpers
 
 (ert-deftest test-init-rails ()
   (should (equal '(() () ()) (init-rails 3))))
@@ -38,11 +96,11 @@
 
 
 (ert-deftest test-add-to-3rd ()
-  (should (equal '(("B") ("C" "D") ("W" "A") ("C" "D"))
+  (should (equal '(("B" ".") ("C" "D" ".") ("W" "A") ("C" "D" "."))
                  (add "A" 2'(("B") ("C" "D") ("W") ("C" "D"))))))
 
 (ert-deftest test-add-to-1st ()
-  (should (equal '(("B" "A") ("C") ())
+  (should (equal '(("B" "A") ("C" ".") ("."))
                  (add "A" 0 '(("B") ("C") ())))))
 
 (ert-deftest test-add-no-add ()
--- rail-cipher/rail-cipher.el
+++ rail-cipher/rail-cipher.el
@@ -4,9 +4,11 @@
 
 
 ;;; Code:
+(setq QMARK "?"
+      DMARK ".")
+
 (defun encode (n src)
-  "Encode src (clear text) using rail-fence cipher using n rails
-"
+  "Encode src (clear text) using rail-fence cipher using n rails"
   (cond
    ((= (length src) 0) src)
    ((or (< n 1) (>= n (length src)))
@@ -16,28 +18,54 @@
                  (cat-rails (encode-fn n src)) "")))
   )
 
-(defun encode-fn (n src)
-  (let* ((txt-list (delete ""
-                           (split-string (replace-regexp-in-string "[^[:alpha:]]" "" src) "")))
+(defun decode (n src)
+  "Decode src (ciphered text) using n rails"
+  (cond ((= 0 (length src)) src)
+        ((< n 1) (throw 'Error "n must be such that 1 < n < length(src)"))
+        ((= n 1) src)
+        (t (decode-fn n src)))
+  )
+
+(defun encode-fn (n src &optional switch)
+  "
+encode-fn can be used to encode a clear text to its encoded form, but it can also be used
+to expand the special mark ? for the decoding portion.
+
+Optional argument switch is used to toggle the behaviour of encode-fn:
+  - if nil => encoding
+  - otherwise spread \"?\"
+"
+  (let* ((txt-list (word-to-list src))
          (rails (init-rails n))
          (rix 0)
          (desc t)
          (op-fn '+)
+         ;; closure:
          (distribute (lambda (l)
                        (progn
-                         (setq rails (add l rix rails)  ;; add letter to rix-th rails
-                               rix (update-ix rix op-fn))
-                         (cond
-                          ((and (= n rix) desc) (setq desc nil
-                                                      rix (- rix 2)
-                                                      op-fn '-))
-                          ((and (= -1 rix) (not desc)) (setq desc t
-                                                             rix 1
-                                                             op-fn '+))
-                          (t t))))))
+                         (and (not (equal switch nil)) (setq l QMARK)) ;; overwrite l
+                         (setq rails (add l rix rails)                 ;; add letter to rix-th rails, dot to remaining rails
+                               triplet (update-ix rix n op-fn desc)    ;; update rix and change op and direction accordingly
+                               rix (car triplet)
+                               op-fn (cadr triplet)
+                               desc (caddr triplet)))
+                       )))
     (progn
       (mapcar distribute txt-list)
       rails)
+    )
+  )
+
+(defun decode-fn (n src)
+  (let* ((l-src (word-to-list src))
+         (rails (encode-fn n src t))
+         (n-rails (replace-mark rails l-src)))
+    ;;
+    (mapconcat 'identity
+               (mapcar 'car
+                       (mapcar 'squeeze-rail
+                               (transpose n-rails)))
+               "")
     )
   )
 
@@ -50,40 +78,125 @@
     rails)
   )
 
-(defun update-ix (ix op)
-  "op is assumed to be either '- or '+"
-  (apply op (list ix 1))
-  )
+(defun update-ix (ix n op desc)
+  "op is assumed to be either '- or '+
+return triplet <updated ix, op and desc>
+"
+  (let ((rix (apply op (list ix 1))))
+    (cond
+     ((and (= n rix) desc) (list (- rix 2) '- nil))
+     ((and (= -1 rix) (not desc)) (list 1 '+ t))
+     ;; no change
+     (t (list rix op desc)))
+  ))
 
 (defun add (l ix rails)
+  "Add letter l to ix-th rails, while filling the others with \".\""
   (let ((jx 0)
         (n (length rails))
         (n-rails '()))
-
     (if (>= ix n) rails
       ;; else
       (while (< jx n)
         (if (= ix jx)
-            (setq nl (append (car rails) (list l))
-                  n-rails (append n-rails (list nl)))
-          (setq n-rails (append n-rails (list (car rails)))))
-        (setq jx (1+ jx)
+            (setq nl (append (car rails) (list l)))
+          ;; else
+          (setq nl (append (car rails) (list DMARK))))
+        (setq n-rails (append n-rails (list nl))
+              jx (1+ jx)
               rails (cdr rails))
         )
       n-rails)
       )
   )
 
+(defun squeeze-rail (rail &optional char)
+  "Delete all occurences of char (default is \".\") in rails"
+  (progn
+    (and (equal nil char) (setq char DMARK))
+    (delete char rail)))
+
 (defun cat-rails (rails)
   (let ((n-rails '()))
     (while (> (length rails) 0)
-      (setq n-rails (append n-rails (car rails))
+      (setq n-rails (append n-rails (squeeze-rail (car rails)) )
             rails (cdr rails)))
     n-rails))
 
-;; TODO
-(defun decode (l ix rails)
+(defun replace-mark (rails src)
+  "Replace mark on each given rails by assigning the letters from src in order...
+Returns the updated rails
+"
+  (let ((ix 0)
+        (n (length rails))
+        (n-rails '()))
+    (while (< ix n)
+      (setq tuple (replace-mark-fn (nth ix rails) src '()) ;; (replace-mark-fn (nth ix rails) src '() '())
+            src (car tuple)
+            n-rails (append n-rails (cdr tuple))
+            ix (1+ ix)))
+    n-rails
+    ))
+
+(defun replace-mark-fn (rail src n-rail)
+  "Returns a list of 2 sub-lists"
+  (cond ((= 0 (length rail)) (list src n-rail))
+        ;; match
+        ((equal QMARK (car rail))
+         (replace-mark-fn (cdr rail)
+                          (cdr src)
+                          (append n-rail (list (car src)))))
+        ;; non-match
+        (t (replace-mark-fn (cdr rail)
+                            src
+                            (append n-rail (list DMARK)))))
   )
+
+;; (defun replace-mark-fn (rail src n-rail n-src)
+;;   "Returns a list of 2 sub-lists"
+;;   (cond ((= 0 (length rail)) (list n-src n-rail))
+;;         ;; match
+;;         ((equal QMARK (car rail))
+;;          (replace-mark-fn (cdr rail)
+;;                           (cdr src)
+;;                           (append n-rail (list (car src)))
+;;                           (append n-src (list DMARK))))
+;;         ;; non-match
+;;         (t (replace-mark-fn (cdr rail)
+;;                             (cdr src)
+;;                             (append n-rail (list DMARK))
+;;                             (append n-src (list (car src))))))
+;;   )
+
+(defun word-to-list (src)
+  (delete ""
+          (split-string (replace-regexp-in-string "[^[:alnum:]]" "" src) ""))
+  )
+
+(defun transpose (llst)
+  "From '((\"F\" \".\" \"O\" \".\" \"A\" \".\")
+          (\".\" \"O\" \".\" \"B\" \".\" \"R\")))
+To:
+   ((\"F\" \".\")
+    (\".\" \"O\")
+    (\"O\" \".\")
+    (\".\" \"B\")
+    (\"A\" \".\")
+    (\".\" \"R\"))
+"
+  (if (= 0 (length llst)) llst
+    (let* ((n (length (car llst))) ;; all sublists of llst assumed to be of same length
+          (ix 0)
+          (r-lst '())
+          (n-th (lambda (lst)
+                  (nth ix lst))))
+      (while (< ix n)
+        (setq s-lst (mapcar n-th llst)
+              ix (1+ ix)
+              r-lst (append r-lst (list s-lst)))
+        )
+      r-lst
+      )))
 
 (provide 'rail-cipher)
 ;;; rail-cipher.el ends here
