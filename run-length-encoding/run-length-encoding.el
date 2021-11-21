fe01b0d37575ce7a983ff966568e51a8b704f788
Pascal P <lacsap_666@yahoo.fr>
2021-11-21 17:50:03 +1300

    run-length-encoding: review

--- run-length-encoding/run-length-encoding.el
+++ run-length-encoding/run-length-encoding.el
@@ -7,25 +7,31 @@
   (cond
    ((= 0 (length str)) str)
    ((= 1 (length str)) str)
-   (t (rle () (delete "" (split-string str "")) ()))
+   (t (rle () (ya:split-string str) ()))
    ))
 
-(defun run-length-decode (str)
-  (rld (delete "" (split-string str "")) ())
-  )
+(defsubst run-length-decode (str)
+  (rld (ya:split-string str) ()))
 
 (defun rle (curr lst nlst)
   (cond
-   ((= 0 (length lst)) (join (append nlst (list (enc-char curr))) ""))
-   ((string= (car curr) (car lst)) (rle (update-pair curr) (cdr lst) nlst))
-   ((equal nil curr) (rle (cons (car lst) 1) (cdr lst) nlst))
-   (t (rle (cons (car lst) 1) (cdr lst) (append nlst (list (enc-char curr)))))
+   ((null lst)
+    (join (append nlst (list (enc-char curr))) ""))
+   ((string= (car curr) (car lst))
+    (rle (update-pair curr) (cdr lst) nlst))
+   ((equal nil curr)
+    (rle (cons (car lst) 1) (cdr lst) nlst))
+   (t
+    (rle (cons (car lst) 1) (cdr lst) (append nlst (list (enc-char curr)))))
    ))
 
 (defun rld (lst nlst)
   (cond
-   ((= 0 (length lst)) (join nlst ""))
-   ((= 1 (length lst)) (join (append nlst (list (car lst))) ""))
+   ((null lst)
+    (join nlst ""))
+   ((= 1 (length lst))
+    (join (append nlst (list (car lst))) ""))
+   ;;
    ;; at least length 2
    (t (let* ((pair (build-list-num lst))
              (num (string-to-number (cdr pair)))
@@ -36,18 +42,15 @@
         )))
    ))
 
-(defun update-pair (pair)
-  (let* ((val (cdr pair))
-         (nval (1+ val)))
-    (cons (car pair) nval)
-    ))
+(defsubst update-pair (pair)
+  (cons (car pair) (1+ (cdr pair))))
 
 (defun enc-char (pair)
   "Example: pair == (\"X\" . 2) => 2X "
   (cond
    ((> (cdr pair) 1) (concat (number-to-string (cdr pair)) (car pair)))
    ((= (cdr pair) 0) "")
-   ((< (cdr pair) 0) (throw 'Error "caount must not me negative"))
+   ((< (cdr pair) 0) (throw 'Error "count must not me negative"))
    (t (car pair))
    ))
 
@@ -57,23 +60,24 @@
    ((= 0 (string-to-number (car lst))) (cons lst "0"))
    (t (let ((cnum  (car lst))
             (c-lst (cdr lst)))
-        (while (and (not (empty-list? c-lst))
+        (while (and (not (null c-lst))
                     (> (string-to-number (car c-lst)) 0))
-          (setq cnum (concat cnum (car c-lst)))
-          (setq c-lst (cdr c-lst))
+          (setq cnum (concat cnum (car c-lst))
+                c-lst (cdr c-lst))
           )
         (cons c-lst cnum)))
    ))
 
-(defun empty-list? (lst)
-  (= 0 (length lst)))
+(defsubst ya:split-string (str &optional ch)
+  (unless ch (setq ch ""))
+  (delete "" (split-string str ch))
+  )
 
-(defun join (lst sep)
+(defsubst join (lst sep)
   (mapconcat 'identity lst sep))
 
-(defun repeat-char (str num)
-  (make-string num (string-to-char str))
-  )
+(defsubst repeat-char (str num)
+  (make-string num (string-to-char str)))
 
 (provide 'run-length-encoding)
 ;;; run-length-encoding.el ends here
