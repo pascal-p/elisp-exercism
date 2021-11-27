eea7ad1aea28c7cbb4d12ac138f030b7edcbfa64
Pascal P <lacsap_666@yahoo.fr>
2021-11-28 11:33:52 +1300

    2nd iter: complement of a function

--- hof/hof-test.el
+++ hof/hof-test.el
@@ -7,22 +7,25 @@
 ;; (require 'cl-lib)
 (load-file "hof.el")
 
-(defun odd-pred (x)
-  (= 1 (% x 2)))
+;; (defun odd-pred (x)
+;;   (= 1 (% x 2)))
 
 (ert-deftest test-remove-if ()
-  (should (equal (cl-remove-if 'odd-pred '(10 11 20 21 30 31 40 41)) ;; remove odd numbers
+  (should (equal (cl-remove-if 'ya:odd-pred '(10 11 20 21 30 31 40 41)) ;; remove odd numbers
                  '(10 20 30 40))))
 
 (ert-deftest test-expansion-remove-if-not ()
-    (should (equal (macroexpand '(remove-if-not 'odd-pred '(10 11 20 21 30 31 40 41)))
-                   '(cl-remove-if (lambda (x) (not (funcall 'odd-pred x))) '(10 11 20 21 30 31 40 41))
+    (should (equal (macroexpand '(ya:remove-if-not 'ya:odd-pred '(10 11 20 21 30 31 40 41)))
+                   '(cl-remove-if (lambda (x) (not (funcall 'ya:odd-pred x))) '(10 11 20 21 30 31 40 41))
                    )))
 
 (ert-deftest test-remove-if-not ()
- (should (equal (remove-if-not 'odd-pred '(10 11 20 21 30 31 40 41)) ;; remove not odd numbers ie. remove even numbers
+ (should (equal (ya:remove-if-not 'ya:odd-pred '(10 11 20 21 30 31 40 41)) ;; remove not odd numbers ie. remove even numbers
                 '(11 21 31 41))))
 
+(ert-deftest test-remove-if-not-b ()
+ (should (equal (ya:remove-if-not-b 'ya:odd-pred '(10 11 20 21 30 31 40 41)) ;; remove not odd numbers ie. remove even numbers
+                '(11 21 31 41))))
 
 
 (provide 'hof-test)
--- hof/hof.el
+++ hof/hof.el
@@ -5,12 +5,23 @@
 ;;; Code:
 (require 'cl-lib)
 
-(defun odd-pred (x)
+(defun ya:odd-pred (x)
   (= 1 (% x 2)))
 
-(defmacro remove-if-not (pred-fn &rest args)
+(defmacro ya:remove-if-not (pred-fn &rest args)
   "build the complementary function of remove-if"
   `(cl-remove-if (lambda (x) (not (funcall ,pred-fn x))) ,@args))
+
+(defmacro ya:complement (fn)
+  "defines the complement of a given function"
+  `(lambda (&rest args) (not (apply ,fn args))))
+
+(defalias 'ya:~ 'ya:complement)
+
+;; now we can rewrite remove-if-not in term of complement
+(defmacro ya:remove-if-not-b (pred-fn &rest args)
+  `(cl-remove-if (ya:~ ,pred-fn) ,@args)
+  )
 
 (provide 'hof)
 ;;; hof.el ends here
