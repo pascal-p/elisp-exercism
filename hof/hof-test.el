263dcc8188bc361986d54eded35334e990810359
Pascal P <lacsap_666@yahoo.fr>
2021-11-28 12:03:02 +1300

    3rd iter: destructive version of non-destructive function

--- hof/hof-test.el
+++ hof/hof-test.el
@@ -27,6 +27,24 @@
  (should (equal (ya:remove-if-not-b 'ya:odd-pred '(10 11 20 21 30 31 40 41)) ;; remove not odd numbers ie. remove even numbers
                 '(11 21 31 41))))
 
+(ert-deftest test-remove-if-destructive-ver ()
+  (should (equal (funcall (! 'cl-remove-if) 'ya:odd-pred '(10 11 20 21 30 31 40 41)) ;; remove odd numbers
+                 '(10 20 30 40))))
+
+;; using alias ya:remove-if!
+(ert-deftest test-remove-if! ()
+  (should (equal (ya:remove-if! 'ya:odd-pred '(10 11 20 21 30 31 40 41)) ;; remove odd numbers
+                 '(10 20 30 40))))
+
+;; using alias ya:append!
+(ert-deftest test-append! ()
+  (should (equal (ya:append! '(10 20 30 40) '(11 21 31 41))
+                 '(10 20 30 40 11 21 31 41))))
+
+;; using alias ya:reverse!
+(ert-deftest test-reverse! ()
+  (should (equal (ya:reverse! '(10 20 30 40 50 60))
+                 '(60 50 40 30 20 10))))
 
 (provide 'hof-test)
 ;;; hof-test.el ends here
--- hof/hof.el
+++ hof/hof.el
@@ -23,5 +23,28 @@
   `(cl-remove-if (ya:~ ,pred-fn) ,@args)
   )
 
+;; build an operator to return destrcutive counter-part of a non destructive function
+;; non destructive | destructive
+;;  cl-remove-if   | cl-delete-if
+;;  reverse        | nreverse
+;;  append         | nconc
+
+(defvar *!store-fns* (make-hash-table))
+
+(defmacro ! (fn)
+  "retrieve destructive alias from *!store-fns*"
+  `(or (gethash ,fn *!store-fns*) ,fn))
+
+(defmacro def! (fn fn!)
+  `(setf (gethash ,fn *!store-fns*) ,fn!))
+
+(def! 'cl-remove-if 'cl-delete-if)  ;; now we can use (! cl-remove-if) (as destructive equivalnet of cl-remove-if instead of ...)
+(def! 'reverse 'nreverse)
+(def! 'append 'nconc)
+
+(defalias 'ya:remove-if! (! 'cl-remove-if))
+(defalias 'ya:reverse! (! 'reverse))
+(defalias 'ya:append! (! 'append))
+
 (provide 'hof)
 ;;; hof.el ends here
